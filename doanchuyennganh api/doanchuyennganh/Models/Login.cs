46f9c765ba05cd5fb6b1ca43e79dca151a8a6052
HTT <44412578+TTHuong@users.noreply.github.com>
2021-01-06 23:04:23 +0700

    up lan dau

---
+++ doanchuyennganh api/doanchuyennganh/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace doanchuyennganh.Models
+{
+    public class Login
+    {
+        public string Accout { get; set; }
+        public string Password { get; set; }
+    }
+}
